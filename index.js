048f616fe4e1852f95448453f00d31edb1768241
takbirgazi <takbirgazibd@gmail.com>
2024-08-27 15:46:05 +0600

    make an API for file Upload

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const multer = require('multer');
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+app.use(express.json());
+app.use(cors());
+
+
+const uploadPath = "C:/Users/user/Desktop/server/assets/image"; // Current Location / Path of Your Project.
+// const imageApi = "C:/Users/user/Desktop/server/assets/image/my-image-file_dateVal_1724750054595_rickshaw.jpg";
+
+app.get("/", (req, res) => {
+    res.send("Welcome to my website.");
+});
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, uploadPath)
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${file.fieldname}_dateVal_${Date.now()}_${file.originalname}`)
+    }
+});
+
+const imageUpload = multer({ storage: storage });
+
+app.post('/upload', imageUpload.array("my-image-file"), (req, res) => {
+    console.log('POST request received to /upload.');
+    console.log('Axios POST body: ', req.body);
+    res.send('Upload Successfully!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at ${port}`);
+})
+
+
+
+
+
+
+/* ----------------------------------------------------------------------------------------------------------------
+ * Use This Code in your React Client Side 
+ 
+import axios from 'axios';
+import React, { useState } from 'react';
+
+function Upload() {
+    const [image, setImage] = useState(null);
+    const handleClick = () => {
+        axios.post('http://localhost:5000/upload', image)
+            .then(res => {
+                console.log('Axios response: ', res)
+            })
+    }
+    const handleFileInput = (e) => {
+        console.log(e.target.files[0]);
+        const formData = new FormData();
+        formData.append('my-image-file', e.target.files[0], e.target.files[0].name);
+        setImage(formData);
+        console.log(image);
+    }
+    console.log(image);
+
+    return (
+        <div className="App">
+            <button onClick={handleClick}>Upload!</button>
+            <input type="file" onChange={handleFileInput} />
+        </div>
+    );
+}
+
+export default Upload;
+
+-----------------------------------------------------------------------------------------------------------------*/
